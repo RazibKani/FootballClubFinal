bbfe47ca89a8f32e3ce1b10cc43fc55ff0a144f3
Razib Kani Maulidan <razibkani@gmail.com>
2018-06-13 22:38:28 +0700

    + update test case

--- app/src/androidTest/java/com/razibkani/footballclubfinal/ui/homepage/HomepageActivityTest.kt
+++ app/src/androidTest/java/com/razibkani/footballclubfinal/ui/homepage/HomepageActivityTest.kt
@@ -1,7 +1,21 @@
 package com.razibkani.footballclubfinal.ui.homepage
 
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.RootMatchers.withDecorView
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.razibkani.footballclubfinal.R.id.*
+import com.razibkani.footballclubfinal.ui.matches.prevmatch.PrevMatchViewHolder
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,9 +30,7 @@ class HomepageActivityTest {
 
     @Test
     fun testAppBehaviour() {
-        /*onView(withId(navigation)).check(ViewAssertions.matches(isDisplayed()))
-
-        onView(withId(loadingIndicator)).check(matches(ViewMatchers.isDisplayed()))
+        onView(withId(navigation)).check(ViewAssertions.matches(isDisplayed()))
 
         try {
             Thread.sleep(5000)
@@ -27,15 +39,15 @@ class HomepageActivityTest {
         }
 
         onView(withId(listPrevMatch)).check(ViewAssertions.matches(isDisplayed()))
-        onView(withId(listPrevMatch)).perform(RecyclerViewActions.scrollToPosition<PrevMatchViewHolder>(3))
-        onView(withId(listPrevMatch)).perform(RecyclerViewActions.actionOnItemAtPosition<PrevMatchViewHolder>(3, click()))
+        onView(withId(listPrevMatch)).perform(RecyclerViewActions.scrollToPosition<PrevMatchViewHolder>(7))
+        onView(withId(listPrevMatch)).perform(RecyclerViewActions.actionOnItemAtPosition<PrevMatchViewHolder>(7, click()))
 
         onView(withId(add_to_favorite)).check(matches(isDisplayed()))
         onView(withId(add_to_favorite)).perform(click())
-        onView(withText("Added to Favorite")).inRoot(withDecorView(not(`is`(activityRule.activity.window.decorView)))).check(matches(isDisplayed()))
-        pressBack()
+        onView(ViewMatchers.withText("Added to Favorite")).inRoot(withDecorView(not(`is`(activityRule.activity.window.decorView)))).check(matches(isDisplayed()))
+        Espresso.pressBack()
 
         onView(withId(navigation)).check(ViewAssertions.matches(isDisplayed()))
-        onView(withId(navigation_favorite_match)).perform(click())*/
+        onView(withId(navigation_favorite)).perform(click())
     }
 }
